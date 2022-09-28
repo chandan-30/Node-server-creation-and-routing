8448c50d4d2e57577f0957656019a69d95f2547e
chandan-30 <saichandhan.yata44@gmail.com>
2022-09-28 11:57:33 +0530

    Pushing to github

---
+++ server.js
@@ -0,0 +1,40 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req,res)=>{
+    res.setHeader('Content-Type','text/html');
+
+    let path = './views';
+    switch(req.url){
+        case '/':
+            path += '/index.html';
+            res.statusCode = 200;
+            break;
+        case '/about':
+            path += '/about.html';
+            res.statusCode = 200;
+            break;
+        case '/about-me':
+            res.setHeader('Location','/about');
+            path += '/about.html';
+            res.statusCode = 301;
+            break;
+        default:
+            path += '/404.html';
+            res.statusCode = 404;
+            break;
+    }
+
+    fs.readFile(path, (err,data)=>{
+        if(err) console.log(err);
+        else{
+             res.end(data);
+        }
+    });
+
+
+});
+
+server.listen('3000','localhost',()=>{
+    console.log('listening a port 3000');
+});
